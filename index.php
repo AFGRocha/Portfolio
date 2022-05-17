d1bfcd5c8036301934c6a26cd125d4e77745e9c1
António Rocha <antoniolaranjinha@hotmail.com>
2022-05-17 23:52:52 +0100

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-include ("includes/header.php");
-// Grab inputs
-$page = $_GET[page];
-if ($page=="") {
-	include("index.html"); 
-} else { include ("$page".".php"); } 
-include ("includes/footer.php");
-?>
