381556d326d10679584960b28f5112049b78ef20
António Rocha <afgrocha98@gmail.com>
2024-03-05 14:31:34 +0000

    deleted everything to make new website

--- pages/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-include ("includes/header.php");
-// Grab inputs
-$page = $_GET[page];
-if ($page=="") {
-	include("home.html"); 
-} else { include ("$page".".php"); } 
-include ("includes/footer.php");
-?>
